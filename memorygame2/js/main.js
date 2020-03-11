bf383e868497181bde93c8bd1fda852c7b844ce6
Te Hamilton <tehamilton@Tes-MacBook-Pro.home>
2020-03-11 14:25:59 -0400

    created game logic

--- memorygame2/js/main.js
+++ memorygame2/js/main.js
@@ -1,7 +1,32 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+//console.log("Up and running!");
+//let cardOne = "queen";
+//let cardTwo = "queen";
+//let cardThree = "king";
+//let cardFour = "king";
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardFour);
+
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+let cardTwo = cards[2]
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2) {
+	
+	if (cardsInPlay[0] === cards[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
