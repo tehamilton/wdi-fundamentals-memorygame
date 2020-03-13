72bef5ac2db4b8bc2d3564a95f3a23eeb4714591
Te Hamilton <tehamilton@tes-macbook-pro.home>
2020-03-12 22:28:19 -0400

    created  game logic

--- memorygame2/js/main.js
+++ memorygame2/js/main.js
@@ -6,8 +6,28 @@
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardFour);
 
-let cards = ["queen", "queen", "king", "king"];
-
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -18,20 +38,15 @@ function checkForMatch(){
 }
 
 function flipCard(cardId) {	
-console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[0, 2]);	
+console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
 checkForMatch();
 
-//let cardOne = cards[0];
-//cardsInPlay.push(cardOne);
-//console.log("User flipped " + cardOne);
-
-//let cardTwo = cards[2]
-//cardsInPlay.push(cardTwo);
-//console.log("User flipped " + cardTwo)
 
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cards[2]) {
